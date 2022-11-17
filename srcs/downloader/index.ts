03799937733f79bd53f116e181e47f5bd17293a7
Thomas MEYER <tmeyer@student.42.fr>
2022-11-17 22:48:58 +0100

    Cleaning code

--- srcs/downloader/index.ts
+++ srcs/downloader/index.ts
@@ -10,26 +10,26 @@ import {downloadHttps} from "./protocols/https";
  */
 const downloader = (url: string, destination: string, name: string, verbose: boolean = false): Promise<number> => {
     return new Promise<number>((resolve, reject) => {
-        validateArgs(url, destination, name).then(async options => {
-            try {
-                switch (options.protocol) {
-                    case 'http:':
-                        await downloadHttp(url, destination, name, verbose);
-                        break;
-                    case 'https:':
-                        await downloadHttps(url, destination, name, verbose);
-                        break;
-                    //if we wanted:
-                    // case 'ftp:':
-                    //     await downloadFtp(url, destination, name, verbose, port, username, password);
-                    //     break;
+        validateArgs(url, destination, name)
+            .then(async options => {
+                try {
+                    switch (options.protocol) {
+                        case 'http:':
+                            await downloadHttp(url, destination, name, verbose);
+                            break;
+                        case 'https:':
+                            await downloadHttps(url, destination, name, verbose);
+                            break;
+                        //if we wanted:
+                        // case 'ftp:':
+                        //     await downloadFtp(url, destination, name, verbose, port, username, password);
+                        //     break;
+                    }
+                    resolve(0);
+                } catch (e) {
+                    reject(e);
                 }
-                resolve(0);
-            } catch (e) {
-                reject(e);
-            }
-        })
-            .catch((reason) => reject(reason));
+        }).catch((reason) => reject(reason));
     });
 }
 
