476fde161d50bdf32281e35dd914f286ffc0b131
Thomas MEYER <tmeyer@student.42.fr>
2022-11-17 22:47:44 +0100

    Initial commit

--- srcs/downloader/index.ts
+++ srcs/downloader/index.ts
@@ -1,13 +1,13 @@
+import {validateArgs} from "./validation/validation";
+import {downloadHttp} from "./protocols/http";
+import {downloadHttps} from "./protocols/https";
+
 /***
  * @param url Location of the file to download
  * @param destination Destination folder of the file to download
  * @param name Final name of the file to download
  * @param verbose Werther to print logs or not. Default to false because not needed in most of the cases.
  */
-import {validateArgs} from "./validation/validation";
-import {downloadHttp} from "./protocols/http";
-import {downloadHttps} from "./protocols/https";
-
 const downloader = (url: string, destination: string, name: string, verbose: boolean = false): Promise<number> => {
     return new Promise<number>((resolve, reject) => {
         validateArgs(url, destination, name).then(async options => {
